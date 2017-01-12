d3d2492e421231914a4c1fa0c5720276c556302a
unknown <adityak@LAB00W7FCA-15.unisuper.lan>
2017-01-13 10:28:50 +1100

    java

---
+++ Java/JDI-Examples-master/src/test/java/org/mytests/tests/SmokeTest.java
@@ -0,0 +1,33 @@
+package org.mytests.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import static org.mytests.uiobjects.EpamSite.*;
+import static org.mytests.enums.HeaderMenu.CAREERS;
+
+/**
+ * Created by Alexander_Petrovskiy on 5/23/2016.
+ */
+public class SmokeTest extends InitTests {
+
+    @BeforeMethod
+    public void before(Method method) throws IOException {
+        homePage.isOpened();
+    }
+
+    @Test
+    public void simpliestTest() {
+        homePage.checkOpened();
+    }
+
+    @Test
+    public void oneActionTest() {
+        headerMenu.select(CAREERS);
+        careerPage.checkOpened();
+    }
+
+}
