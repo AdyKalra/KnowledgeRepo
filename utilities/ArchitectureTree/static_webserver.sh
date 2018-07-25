11ef577e5d20fb8b65794fe9d46841c22b1ee0e6
Aditya Kalra <aditya_kalra@ymail.com>
2018-07-25 14:00:02 +1000

    adding timeline

---
+++ utilities/ArchitectureTree/static_webserver.sh
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------------------
+# Start an HTTP server from a directory, optionally specifying the port
+# ----------------------------------------------------------------------
+
+pyserver() {
+    sleep 1 && python -m http.server 4000
+    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+    #python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
